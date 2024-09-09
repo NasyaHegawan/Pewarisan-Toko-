34ebbb20cb37f5985edf3d8fb306783121f875c8
NasyaHegawan <nasya_putri_33rpl@student.smktelkom-mlg.sch.id>
2024-09-09 14:18:30 +0700

    Add files via upload

---
+++ TesterCD.java
@@ -0,0 +1,18 @@
+package Pewarisan;
+
+public class TesterCD {
+    public static void main(String[] args) {
+        CD a = new CD();
+
+        a.setNumber(2);
+        a.setName("Reborn");
+        a.setQuantity(12);
+        a.setPrice(2000.0);
+        a.setArtist("Lady Gaga");
+        a.setNumSong(10);
+        a.setLabel("Sony Music");
+        
+        a.print();
+
+    }
+}
